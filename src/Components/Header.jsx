38cf510502e03cd9178cf717fbefeee7e9e4fd52
Dennisonya <155449946+Dennisonya@users.noreply.github.com>
2024-08-06 23:53:00 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function Header (){
     return(
         <div className="header">
-        <img src="src/assets/clipboard.png" />
         <h1>CV APPLICATION</h1>
         </div>
     )
